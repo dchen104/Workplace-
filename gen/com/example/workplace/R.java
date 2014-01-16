357681737de8f71022c6ad8a421d603995e4b3a8
dchen104 <dchen104@gmail.com>
2014-01-15 21:03:45 -0500

    Added CreateNetwork screen functionality :

--- gen/com/example/workplace/R.java
+++ gen/com/example/workplace/R.java
@@ -24,28 +24,31 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int GridLayout1=0x7f080000;
-        public static final int TextView01=0x7f08000d;
-        public static final int action_settings=0x7f080015;
-        public static final int confirmPasswordRegister=0x7f08000a;
-        public static final int emailRegister=0x7f080002;
-        public static final int firstRegister=0x7f08000c;
-        public static final int lastRegister=0x7f08000e;
-        public static final int loginButton=0x7f080005;
-        public static final int networkRegister=0x7f08000f;
-        public static final int passwordRegister=0x7f080003;
-        public static final int phoneRegister=0x7f080013;
-        public static final int radioCreate=0x7f080011;
-        public static final int radioJoin=0x7f080010;
-        public static final int registerButton=0x7f080007;
-        public static final int registerConfirmButton=0x7f080014;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080004;
-        public static final int textView3=0x7f080006;
-        public static final int textView4=0x7f080008;
-        public static final int textView5=0x7f080009;
-        public static final int textView6=0x7f08000b;
-        public static final int textView7=0x7f080012;
+        public static final int GridLayout1=0x7f080005;
+        public static final int RelativeLayout1=0x7f080000;
+        public static final int TextView01=0x7f080010;
+        public static final int action_settings=0x7f080018;
+        public static final int confirmPasswordRegister=0x7f08000d;
+        public static final int createNetworkButton=0x7f080004;
+        public static final int emailRegister=0x7f080006;
+        public static final int firstRegister=0x7f08000f;
+        public static final int lastRegister=0x7f080011;
+        public static final int loginButton=0x7f080008;
+        public static final int networkRegister=0x7f080012;
+        public static final int newNetworkName=0x7f080001;
+        public static final int passwordRegister=0x7f080007;
+        public static final int phoneRegister=0x7f080016;
+        public static final int radioCreate=0x7f080014;
+        public static final int radioJoin=0x7f080013;
+        public static final int registerButton=0x7f08000a;
+        public static final int registerConfirmButton=0x7f080017;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080003;
+        public static final int textView3=0x7f080009;
+        public static final int textView4=0x7f08000b;
+        public static final int textView5=0x7f08000c;
+        public static final int textView6=0x7f08000e;
+        public static final int textView7=0x7f080015;
     }
     public static final class layout {
         public static final int activity_create_network=0x7f030000;
--- src/com/example/workplace/CreateNetwork.java
+++ src/com/example/workplace/CreateNetwork.java
@@ -1,15 +1,73 @@
 package com.example.workplace;
 
+import java.util.List;
+
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import android.net.ParseException;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateNetwork extends Activity {
+	
+	String networkName;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_network);
+		
+		final Button createNetworkButton = (Button) findViewById(R.id.createNetworkButton);
+		createNetworkButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				AlertDialog.Builder waitDialog = new AlertDialog.Builder(CreateNetwork.this);
+				waitDialog.setMessage("Please wait...");
+				AlertDialog waitPop = waitDialog.create();
+				waitPop.show();
+				networkName = ((EditText) findViewById(R.id.newNetworkName)).getText().toString();
+				if(!uniqueNetwork()) {
+					waitPop.cancel();
+					AlertDialog.Builder networkDialog = new AlertDialog.Builder(CreateNetwork.this);
+					networkDialog.setTitle("Error");
+					networkDialog.setMessage("The network name you have entered has already been taken, "
+							+ "please select a new one.");
+					networkDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							((EditText)findViewById(R.id.newNetworkName)).setText("");
+							dialog.cancel();
+						}
+					});
+					AlertDialog emailPop2 = networkDialog.create();
+					networkDialog.show();
+				}
+				else {
+					waitPop.cancel();
+					AlertDialog.Builder networkDialog2 = new AlertDialog.Builder(CreateNetwork.this);
+					networkDialog2.setTitle("Congratulations!");
+					networkDialog2.setMessage("You have successfully created and account and new network"
+							+ ". You may now send out email invitations for coworkers to join the network"
+							+ "or simply wait for coworkers to join the network on their own. You will now"
+							+ "be taken back to the login screen to login.");
+					networkDialog2.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							((EditText)findViewById(R.id.newNetworkName)).setText("");
+							dialog.cancel();
+						}
+					});
+				}
+				
+			}
+			
+		});
+			
 	}
 
 	@Override
@@ -17,6 +75,25 @@ public class CreateNetwork extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.create_network, menu);
 		return true;
+	}
+	
+	private boolean uniqueNetwork() {
+		ParseQuery<ParseObject> findNetQuery = ParseQuery.getQuery("Networks");
+		findNetQuery.whereEqualTo("name", networkName);
+		try {
+			List<ParseObject> list;
+
+			list = findNetQuery.find();
+
+			if (list.isEmpty()) {
+				return true;
+			}
+			else return false;
+		}
+		catch (com.parse.ParseException e) {
+			return false;
+		}
+		
 	}
 
 }
